2e8c7f289918d1c5dc0b18879e7bde6fa1ce36ec
Ilia Abolhasani <abolhasani.eliya@gmail.com>
2024-05-22 14:45:12 +0330

    show elipsed time.

--- app/cron/job_connection_analize.py
+++ app/cron/job_connection_analize.py
@@ -4,14 +4,19 @@ from app.cron import job_lock
 
 def start(context, logger_api):
     global job_lock
-    with job_lock:
+    with job_lock:        
         print("job_connection_analize")
         try:
             start_time = time.time()
             context.proxies_connection_update()            
+            end_time = time.time()
+            elapsed_time = end_time - start_time
+            print(f"proxies_connection_update elapsed_time: {elapsed_time}")
+
+            start_time = time.time()
             context.delete_dead_proxies(30)
             end_time = time.time()
             elapsed_time = end_time - start_time
-            print(f"job_connection_analize elapsed_time: {elapsed_time}")
+            print(f"delete_dead_proxies elapsed_time: {elapsed_time}")
         except Exception as error:
             logger_api.announce(error, "Connection analized job.")
