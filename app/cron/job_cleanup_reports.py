84f5d6d17a89bfb58fdc72db56ab2d27b0ea097c
Ilia Abolhasani <abolhasani.eliya@gmail.com>
2024-05-22 13:53:02 +0330

    excute time calc

--- app/cron/job_cleanup_reports.py
+++ app/cron/job_cleanup_reports.py
@@ -6,6 +6,10 @@ def start(context, logger_api):
     with job_lock:
         print("job_cleanup_reports")
         try:
+            start_time = time.time()
             context.cleanup_old_ping_reports()            
+            end_time = time.time()
+            elapsed_time = end_time - start_time
+            print(f"job_cleanup_reports elapsed_time: {elapsed_time}")
         except Exception as error:
             logger_api.announce(error, "job_cleanup_reports.")
--- app/cron/job_connection_analize.py
+++ app/cron/job_connection_analize.py
@@ -6,7 +6,11 @@ def start(context, logger_api):
     with job_lock:
         print("job_connection_analize")
         try:
+            start_time = time.time()
             context.proxies_connection_update()            
             context.delete_dead_proxies(30)
+            end_time = time.time()
+            elapsed_time = end_time - start_time
+            print(f"job_connection_analize elapsed_time: {elapsed_time}")
         except Exception as error:
             logger_api.announce(error, "Connection analized job.")
