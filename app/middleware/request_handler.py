ee7efa3efd647fb0f83d6353fe47513ed8e0bb77
Ilia Abolhasani <abolhasani.eliya@gmail.com>
2024-06-07 13:17:06 +0330

    request bug fixed.

--- app/middleware/request_handler.py
+++ app/middleware/request_handler.py
@@ -1,44 +1,42 @@
 import hashlib
-from flask import abort, request, jsonify
+from flask import abort, request, jsonify, make_response
 from app.Context import Context
 from datetime import datetime, timezone
+
 context = Context()
 
 
 def request_handler_middleware():
     # Exclude the test_route from middleware
-    if request.path == '/favicon.ico':
+    if request.path == "/favicon.ico":
         return None
     if request.endpoint == "route.test_route":
         return None
-    # if (request.view_args):
-    agent_id = request.view_args.get('agent_id')
+    if not request.view_args:
+        abort(404)
+    agent_id = request.view_args.get("agent_id")
     agent = context.get_agent(agent_id)
     if not agent:
-        response = jsonify({"error": "Access denied."})
-        response.status_code = 403
-        return response
-    request_time = request.headers.get('X-Request-Time')
-    hashed_timestamp = request.headers.get('X-Hashed-Timestamp')
+        abort(403)
+    request_time = request.headers.get("X-Request-Time")
+    hashed_timestamp = request.headers.get("X-Hashed-Timestamp")
 
     # Check if request_time and hashed_timestamp are present in the headers
     if not request_time or not hashed_timestamp:
         response = jsonify({"error": "Missing required headers."})
-        response.status_code = 400
-        return response
+        return make_response(response, 400)
 
     # Calculate the hash of the received time
-    message = f'{request_time}{agent.encrypted_key}'
+    message = f"{request_time}{agent.encrypted_key}"
     calculated_hash = hashlib.sha256(message.encode()).hexdigest()
 
     # Check if the calculated hash matches the received hashed_timestamp
     if calculated_hash != hashed_timestamp:
         response = jsonify({"error": "Hash mismatch."})
-        response.status_code = 400
-        return response
+        return make_response(response, 400)
 
     # Convert the received time string to a datetime object
-    received_time = datetime.strptime(request_time, '%Y-%m-%d %H:%M:%S')
+    received_time = datetime.strptime(request_time, "%Y-%m-%d %H:%M:%S")
     received_time = received_time.replace(tzinfo=timezone.utc)
     # Calculate the time difference
     current_time = datetime.now(timezone.utc)
@@ -46,7 +44,6 @@ def request_handler_middleware():
     # Check if the time difference is less than 1 minute (60 seconds)
     if time_difference.total_seconds() > 60:
         response = jsonify({"error": "Request time exceeds 1 minute."})
-        response.status_code = 400
-        return response
+        return make_response(response, 400)
 
     return None
