1fde14514aa746d9c7804a611728d8922a13e205
Ilia Abolhasani <abolhasani.eliya@gmail.com>
2024-06-07 15:07:51 +0330

    cron add message time changed.

--- app/cron/manager.py
+++ app/cron/manager.py
@@ -8,36 +8,35 @@ import app.cron.job_cleanup_reports as job_cleanup_reports
 
 
 def start_jobs(context, telegram_api, bot_api, logger_api):
-    scheduler = BackgroundScheduler(
-        {'apscheduler.job_defaults.max_instances': 5})        
+    scheduler = BackgroundScheduler({"apscheduler.job_defaults.max_instances": 5})
     job_connection_analize.start(context, logger_api)
-    # job add message to channel        
+    # job add message to channel
     scheduler.add_job(
         lambda: job_channel_add_message.start(context, bot_api, logger_api),
-        trigger=CronTrigger.from_crontab('0 */6 * * *')
+        trigger=CronTrigger.from_crontab("0 */12 * * *"),
     )
 
     # job edit last message of channel
     scheduler.add_job(
         lambda: job_channel_edit_message.start(context, bot_api, logger_api),
-        trigger=CronTrigger.from_crontab('*/5 * * * *')
+        trigger=CronTrigger.from_crontab("*/5 * * * *"),
     )
 
     # job test connection of proxy base on reports
     scheduler.add_job(
         lambda: job_connection_analize.start(context, logger_api),
-        trigger=CronTrigger.from_crontab('*/6 * * * *')
+        trigger=CronTrigger.from_crontab("*/6 * * * *"),
     )
 
     # job test connection of proxy base on reports
     scheduler.add_job(
         lambda: job_cleanup_reports.start(context, logger_api),
-        trigger=CronTrigger.from_crontab('*/15 * * * *')
+        trigger=CronTrigger.from_crontab("*/15 * * * *"),
     )
-    
+
     # job fetch new proxies from other proxy chaneels
     scheduler.add_job(
         lambda: job_fetch_new_proxies.start(context, telegram_api, logger_api),
-        trigger=CronTrigger.from_crontab('*/11 * * * *')
+        trigger=CronTrigger.from_crontab("*/11 * * * *"),
     )
     scheduler.start()
