d73828e306d52753a4a0e0e02c1fd4f13a269048
Ilia Abolhasani <abolhasani.eliya@gmail.com>
2024-08-17 20:21:48 +0330

    add message.

--- app/cron/manager.py
+++ app/cron/manager.py
@@ -10,6 +10,7 @@ import app.cron.job_add_csv_report as job_add_csv_report
 
 def start_jobs(context, telegram_api, bot_api, logger_api):
     scheduler = BackgroundScheduler({"apscheduler.job_defaults.max_instances": 5})
+    job_channel_add_message.start(context, bot_api, logger_api)
     # job_add_csv_report.start(context, bot_api, logger_api)
     # job_connection_analize.start(context, logger_api)
     # job_channel_edit_message.start(context, bot_api, logger_api)
