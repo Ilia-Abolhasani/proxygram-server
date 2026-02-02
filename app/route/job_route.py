adb6f86e16884cb7c9cafd141b17e9b3c084bdbb
Ilia Abolhasani <abolhasani.eliya@gmail.com>
2026-02-02 12:20:28 -0800

    edit message api

--- app/route/job_route.py
+++ app/route/job_route.py
@@ -1,5 +1,6 @@
 from flask import Blueprint, jsonify, current_app
 from app.controller.job_controller import JobController
+import app.cron.job_channel_edit_message as job_channel_edit_message
 
 
 from flask import Blueprint, jsonify, request
@@ -14,3 +15,12 @@ def post_log():
     job_controller = JobController(context, logger_api)
     result = job_controller.fetch_new_proxies()
     return jsonify(result), 200
+
+
+@blueprint.route("/edit_channel_message", methods=["GET"])
+def edit_channel_message():
+    context = current_app.config["context"]
+    bot_api = current_app.config["bot_api"]
+    logger_api = current_app.config["logger_api"]
+    job_channel_edit_message.start(context, bot_api, logger_api)
+    return jsonify({"message": "edit_channel_message executed successfully"}), 200
