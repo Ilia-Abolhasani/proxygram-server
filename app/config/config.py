5fd1433fc50bc872451e356f3030504adbbaf3c9
Ilia Abolhasani <abolhasani.eliya@gmail.com>
2026-09-17 10:51:30 -0700

    increase number

--- app/config/config.py
+++ app/config/config.py
@@ -48,7 +48,7 @@ class Config:
     successful_pings = 5
 
     # proxie selection parameters
-    message_limit_proxy = 10
+    message_limit_proxy = 15
     message_limit_proxy_ir = 20
     max_ping_value = 10000
     exponential_decay = 0.9
